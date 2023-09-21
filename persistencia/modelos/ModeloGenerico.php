038ccd3383ffe694824525a001a55c1e24ea4e5b
BryanCachimuel <bryanloyo56@gmail.com>
2023-09-20 21:27:00 -0500

    creando la función obtenerAtributos en donde se obtiene las variables definidas en la clase de Panaderia

--- persistencia/modelos/ModeloGenerico.php
+++ persistencia/modelos/ModeloGenerico.php
@@ -16,6 +16,16 @@
             }
         }
 
-       
+        protected function obtenerAtributos() {
+            $variables = get_class_vars($this->className);
+            $atributos = [];
+            $max = count($variables);
+            foreach($variables as $llave => $valor){
+                if(!in_array($llave, $this->excluir)){
+                    $atributos[] = $llave;
+                }
+            }
+            return $atributos;
+        }
     }
 ?>
