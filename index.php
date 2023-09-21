6f0fd93bc9a07c88be5170d5f186625135a4c4e7
BryanCachimuel <bryanloyo56@gmail.com>
2023-09-20 21:12:06 -0500

    realizando pruebas de funcionamiento de las funciones crud

--- index.php
+++ index.php
@@ -2,9 +2,31 @@
     require_once "./conexion/Conexion.php";
     require_once "./persistencia/Crud.php";
 
-    $crud = new Crud("panaderia");
-    $lista = $crud->get();
+    $crud = new Crud("panaderia"); 
 
+    /*$id = $crud->registrar([
+            "producto" => "empanadas",
+            "descripcion" => "arina de maíz con platano maduro",
+            "cantidad" => 50,
+            "valor_unidad" => 0.25,
+            "fecha_elaboracion" => date("Y-m-d H:i:s")
+        ]);
+    echo "El id insertado es: ". $id;*/
+
+    $filasAfectadas = $crud->where("id", "=", 3)->actualizar([
+        "producto" => "caña",
+        "descripcion" => "arina de trigo con chocolate",
+        "valor_unidad" => 0.35
+    ]);
+
+    $eliminar = $crud->where("id", "=", 1)->eliminar();
+
+    echo "FILAS AFECTADAS: " . $filasAfectadas . "Eliminadas: " . $eliminar;
+    
+    echo "<br/>";
+
+    $lista = $crud->mostrarRegistros();
+    echo "<pre>";
     var_dump($lista);
-
+    echo "</pre>";
 ?>
