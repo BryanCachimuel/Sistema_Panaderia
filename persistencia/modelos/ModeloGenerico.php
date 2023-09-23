35b891815604ffd515daefd7471e802b6a9fca94
BryanCachimuel <bryanloyo56@gmail.com>
2023-09-22 20:58:35 -0500

    corrigiendo el script de algunas funciones descritas en la clase ModeloGenerico

--- persistencia/modelos/ModeloGenerico.php
+++ persistencia/modelos/ModeloGenerico.php
@@ -1,88 +1,90 @@
 <?php
-    class ModeloGenerico extends Crud {
 
-        private $className;
-        private $excluir = ["className", "tabla", "conexion", "wheres", "sql", "excluir"];
+class ModeloGenerico extends Crud {
 
-        function __construct($tabla, $propiedades = null) {
-            parent::__construct($tabla);
-            
-            if(empty($propiedades)){
-                return;
-            }
+    private $className;
+    private $excluir = ["className", "tabla", "conexion", "wheres", "sql", "excluir"];
 
-            foreach($propiedades as $llaves => $valor){
-                $this->{$llaves} = $valor;
-            }
+    function __construct($tabla, $className, $propiedades = null) {
+        parent::__construct($tabla);
+        $this->className = $className;
+
+        if (empty($propiedades)) {
+            return;
         }
 
-        protected function obtenerAtributos() {
-            $variables = get_class_vars($this->className);
-            $atributos = [];
-            $max = count($variables);
-            foreach($variables as $llave => $valor){
-                if(!in_array($llave, $this->excluir)){
-                    $atributos[] = $llave;
-                }
-            }
-            return $atributos;
+        foreach ($propiedades as $llave => $valor) {
+            $this->{$llave} = $valor;
         }
+    }
 
-        protected function parsear($obj = null) {
-            try {
-                $atributos = $this->obtenerAtributos();
-                $objetoFinal = [];
-                // Obtener el objeto desde el modelo
-                if($obj == null){
-                    foreach ($atributos as $indice => $llave) {
-                       if(isset($this->{$llave})){
-                            $objetoFinal[$llave] = $this->{$llave};
-                       }
-                    }
-                    return $objetoFinal;
-                }
+    protected function obtenerAtributos() {
+        $variables = get_class_vars($this->className);
+        $atributos = [];
+        $max = count($variables);
+        foreach ($variables as $llave => $valor) {
+            if (!in_array($llave, $this->excluir)) {
+                $atributos[] = $llave;
+            }
+        }
+        return $atributos;
+    }
 
-                // corregir el objeto que recibimos con los atributos del modelo
+    protected function parsear($obj = null) {
+        try {
+            $atributos = $this->obtenerAtributos();
+            $objetoFinal = [];
+            //Obtenes el objeto desde el modelo.
+            if ($obj == null) {
                 foreach ($atributos as $indice => $llave) {
-                    if(isset($obj[$llave])){
-                        $objetoFinal[$llave] = $obj[$llave];
+                    if (isset($this->{$llave})) {
+                        $objetoFinal[$llave] = $this->{$llave};
                     }
                 }
                 return $objetoFinal;
-            } catch (Exception $e) {
-                throw new Exception("Error en ". $this->className . ".parsear() =>".$e->getMessage());
             }
-        }
 
-        public function fill($obj){
-            try {
-                $atributos = $this->obtenerAtributos();
-                foreach ($atributos as $indice => $llave) {
-                    if(isset($obj[$llave])){
-                        $this->{$llave} = $obj[$llave];
-                    }
+            //Corregir el objeto que recibimos con los atributos del modelo.
+            foreach ($atributos as $indice => $llave) {
+                if (isset($obj[$llave])) {
+                    $objetoFinal[$llave] = $obj[$llave];
                 }
-            } catch (Exception $e) {
-                throw new Exception("Error en ". $this->className . ".fill() =>".$e->getMessage());
             }
-        }
-
-        public function insert($obj = null) {
-            $obj = $this->parsear($obj);
-            return parent::registrar($obj);
-        }
-
-        public function update($obj = null) {
-            $obj = $this->update($obj);
-            return parent::actualizar($obj);
-        }
-
-        public function __get($nombreAtributo) {
-            return $this->{$nombreAtributo};
-        }
-
-        public function __set($nombreAtributo, $valor){
-            $this->{$nombreAtributo} = $valor;
+            return $objetoFinal;
+        } catch (Exception $ex) {
+            throw new Exception("Error en " . $this->className . ".parsear() => " . $ex->getMessage());
         }
     }
-?>
+
+    public function fill($obj) {
+        try {
+            $atributos = $this->obtenerAtributos();
+            foreach ($atributos as $indice => $llave) {
+                if (isset($obj[$llave])) {
+                    $this->{$llave} = $obj[$llave];
+                }
+            }
+        } catch (Exception $ex) {
+            throw new Exception("Error en " . $this->className . ".fill() => " . $ex->getMessage());
+        }
+    }
+
+    public function insert($obj = null) {
+        $obj = $this->parsear($obj);
+        return parent::registrar($obj);
+    }
+
+    public function update($obj) {
+        $obj = $this->parsear($obj);
+        return parent::actualizar($obj);
+    }
+
+    public function __get($nombreAtributo) {
+        return $this->{$nombreAtributo};
+    }
+
+    public function __set($nombreAtributo, $valor) {
+        $this->{$nombreAtributo} = $valor;
+    }
+
+}
