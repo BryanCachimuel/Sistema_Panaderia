5474e7f5c1d3aab91e8d56e6913245254c23f89b
BryanCachimuel <bryanloyo56@gmail.com>
2023-09-22 20:32:54 -0500

    creando el constructor de la clase en donde se define el nombre de la tabla de la base de datos, el nombre de la clase con los atributos de la base de datos y las propiedades, como también se crearon las respectivas funciones get y set de los atributos de la clase Panaderia

--- persistencia/modelos/Panaderia.php
+++ persistencia/modelos/Panaderia.php
@@ -7,8 +7,56 @@
         protected $valor_unidad;
         protected $fecha_elaboracion;
 
-        public function __construct($propiedad = null) {
-            
+        public function __construct($propiedades = null) {
+            parent::__construct("panaderia", Panaderia::class, $propiedades);
+        }
+        
+        function getId() {
+            return $this->id;
+        }
+
+        function getProducto(){
+            return $this->producto;
+        }
+
+        function gesDescripcion(){
+            return $this->descripcion;
+        }
+
+        function getCantidad() {
+            return $this->cantidad;
+        }
+
+        function getValor_Unidad() {
+            return $this->valor_unidad;
+        }
+
+        function getFecha_Elaboracion() {
+            return $this->fecha_elaboracion;
+        }
+
+        function setId($id){
+            $this->id = $id;
+        }
+
+        function setProducto($producto){
+            $this->producto = $producto;
+        }
+
+        function setDescripcion($descripcion){
+            $this->descripcion = $descripcion;
+        }
+
+        function setCantidad($cantidad){
+            $this->cantidad = $cantidad;
+        }
+
+        function setValor_Unidad($valor_unidad){
+            $this->valor_unidad = $valor_unidad;
+        }
+
+        function setFecha_Elaboracion($fecha_elaboracion){
+            $this->fecha_elaboracion = $fecha_elaboracion;
         }
     }
 ?>
