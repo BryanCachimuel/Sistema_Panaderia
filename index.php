a805ad8ef3403164db42fe998a8d7a47305a9dc6
BryanCachimuel <bryanloyo56@gmail.com>
2023-09-25 20:10:26 -0500

    realizando pruebas de funcionamiento de las funciones descritas en la clase ControladorPanaderia y verificando que los procesos de cada función realicen sean correctos

--- index.php
+++ index.php
@@ -1,32 +1,54 @@
 <?php 
     require_once "./conexion/Conexion.php";
     require_once "./persistencia/Crud.php";
+    require_once "./persistencia/modelos/ModeloGenerico.php";
+    require_once "./persistencia/modelos/Panaderia.php";
+    require_once "./http/ControladorPanaderia.php";
 
-    $crud = new Crud("panaderia"); 
-
-    /*$id = $crud->registrar([
-            "producto" => "empanadas",
-            "descripcion" => "arina de maíz con platano maduro",
-            "cantidad" => 50,
-            "valor_unidad" => 0.25,
-            "fecha_elaboracion" => date("Y-m-d H:i:s")
-        ]);
-    echo "El id insertado es: ". $id;*/
-
-    $filasAfectadas = $crud->where("id", "=", 3)->actualizar([
-        "producto" => "caña",
-        "descripcion" => "arina de trigo con chocolate",
-        "valor_unidad" => 0.35
+    $controladorPanaderia = new ControladorPanaderia();
+    /*$respuesta = $controladorPanaderia->insertarPanaderia([
+        "producto" => "leche",
+        "descripcion" => "leche semidescremada",
+        "cantidad" => 25,
+        "valor_unidad" => 1.05
     ]);
 
-    $eliminar = $crud->where("id", "=", 1)->eliminar();
+    echo "</br>";
+    var_dump($respuesta);
+    echo "</br>";*/
 
-    echo "FILAS AFECTADAS: " . $filasAfectadas . "Eliminadas: " . $eliminar;
-    
-    echo "<br/>";
 
-    $lista = $crud->mostrarRegistros();
+
+    echo "</br>";
+
     echo "<pre>";
-    var_dump($lista);
+    $listado = $controladorPanaderia->listarPanaderia();
+    var_dump($listado);
     echo "</pre>";
+
+    echo "</br>";
+
+    
+    echo "<pre>";
+    $panaderiaId = $controladorPanaderia->buscarPanaderiaPorId(2);
+    var_dump($panaderiaId);
+    echo "</pre>";
+
+    echo "</br>";
+
+    echo "<pre>";
+    $actualizarpanaderia = $controladorPanaderia->actualizarPanaderia([
+        "idPanaderia" => 6,
+        "producto" => "Avena Polaca",
+        "descripcion" => "avena natural, azucas y leche",
+        "cantidad" => 25,
+        "valor_unidad" => 1.05
+    ]);
+    var_dump($actualizarpanaderia);
+    echo "</pre>";
+
+    echo "</br>";
+    $borrarpanaderia = $controladorPanaderia->eliminarPanaderia(3);
+    var_dump($borrarpanaderia);
+   
 ?>
