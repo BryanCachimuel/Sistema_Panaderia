8692e56752a27347e5ad2b07190c67a51429c2e6
BryanCachimuel <bryanloyo56@gmail.com>
2023-09-25 22:07:59 -0500

    agregando los mensajes codificados de la clase EMensaje para tener un mejor orden a la hora de ver mensajes de notificaciones por las acciones realizadas por el crud

--- http/ControladorPanaderia.php
+++ http/ControladorPanaderia.php
@@ -8,51 +8,45 @@
         public function insertarPanaderia($panaderia) {
             $panaderiaModel = new Panaderia();
             $id = $panaderiaModel->insert($panaderia);
-            return [
-                "codigo" => (($id > 0) ? 1 : -1),
-                "mensaje" => ($id > 0) ? "Se ha insertado un producto de la panaderia" : "No se a podido insertar el producto de la panaderia",
-                "datos" => $id
-            ];
+            $v = ($id > 0);
+            $respuesta = new Encapsulamiento($v ? EMensajes::INSERCION_EXITOSA : EMensajes::ERROR_INSERSION);
+            $respuesta->setDatos($id);
+            return $respuesta;
         }
 
         public function listarPanaderia() {
             $panaderiaModel = new Panaderia();
             $lista = $panaderiaModel->mostrarRegistros();
-            return [
-                "codigo" => ((count($lista) > 0) ? 1 : -1),
-                "mensaje" => ((count($lista) > 0) ? "Se ha consultado los registros correctamente" : "No hay registros") ,
-                "datos" => $lista
-            ];
+            $v = count($lista);
+            $respuesta = new Encapsulamiento($v ? EMensajes::CORRECTO : EMensajes::ERROR);
+            $respuesta->setDatos($lista);
+            return $respuesta;
         }
 
         public function actualizarPanaderia($panaderia) {
             $panaderiaModel = new Panaderia();
             $actualizados = $panaderiaModel->where("id","=",$panaderia["idPanaderia"])->update($panaderia);
-            return [
-                "codigo" => (($actualizados > 0) ? 1 : -1),
-                "mensaje" => ($actualizados > 0) ? "Se ha actualizado un producto de la panaderia" : "No se a podido actualizar el producto de la panaderia",
-                "datos" => $actualizados
-            ];
+            $v = ($actualizados > 0);
+            $respuesta = new Encapsulamiento($v ? EMensajes::ACTUALIZACION_EXITOSA : EMensajes::ERROR);
+            $respuesta->setDatos($actualizados);
+            return $respuesta;
         }
 
         public function eliminarPanaderia($idPanaderia) {
             $panaderiaModel = new Panaderia();
             $eliminados = $panaderiaModel->where("id","=",$idPanaderia)->eliminar();
-            return [
-                "codigo" => (($eliminados > 0) ? 1 : -1),
-                "mensaje" => ($eliminados > 0) ? "Se ha eliminado un producto de la panaderia" : "No se a podido eliminar el producto de la panaderia",
-                "datos" => $eliminados
-            ];
+            $v = ($eliminados > 0);
+            $respuesta = new Encapsulamiento($v ? EMensajes::ELIMINACION_EXITOSA : EMensajes::ERROR);
+            $respuesta->setDatos($eliminados);
+            return $respuesta;
         }
 
         public function buscarPanaderiaPorId($idPanaderia) {
             $panaderiaModel = new Panaderia();
             $panaderia = $panaderiaModel->where("id","=",$idPanaderia)->mostrarRegistrosUnicos();
-            return [
-                "codigo" => (($panaderia != null) ? 1 : -1),
-                "mensaje" => ($panaderia != null) ? "Se ha consultado un producto de la panaderia" : "No se a podido econtrar el producto de la panaderia",
-                "datos" => $panaderia
-            ];
+            $v = ($panaderia != null);
+            $respuesta = new Encapsulamiento($v ? EMensajes::BUSQUEDA_ID_EXITOSA : EMensajes::ERROR);
+            $respuesta->setDatos($panaderia);
         }
     }
 ?>
