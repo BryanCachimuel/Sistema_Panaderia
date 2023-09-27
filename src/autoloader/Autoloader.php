c173be715c2d995304638cab49d275bdc0fd892b
BryanCachimuel <bryanloyo56@gmail.com>
2023-09-26 20:24:20 -0500

    creando la clase Autoloader en donde se definen varias funciones estaticas entre ellas tenemos la de registrar

---
+++ src/autoloader/Autoloader.php
@@ -0,0 +1,19 @@
+<?php
+
+    class Autoloader {
+
+        public static function registrar(){
+            if(function_exists('__autoload')){
+                spl_autoload_register('__autoload');
+            }
+            /* si da error en la versión poner 5.3.0 */
+            if(version_compare(PHP_VERSION, '8.0.19') >= 0){
+                spl_autoload_register(array('Autoloader','cargar'), true, true);
+            }else{
+                spl_autoload_register(array('Autoloader','cargar'));
+            }
+        } 
+        
+    }
+
+?>
