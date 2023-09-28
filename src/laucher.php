d50935b3a973f15e7e1db7caa270306e1af45430
BryanCachimuel <bryanloyo56@gmail.com>
2023-09-27 21:50:47 -0500

    definiendo la dirección de los archivos de las rutas

---
+++ src/laucher.php
@@ -0,0 +1,13 @@
+<?php
+    require "./src/Roots.php";
+    require PATH_SRC.'autoloader/Autoloader.php';
+    Autoloader::registrar();
+    $rutas = scandir(PATH_ROUTES); /* lista los archivos que se encuentran en el directorio routes */
+    
+    foreach($rutas as $archivo){
+        $rutaArchivo = realpath(PATH_ROUTES. $archivo);
+        if(is_file($rutaArchivo)){
+            require $rutaArchivo;
+        }
+    }
+?>
