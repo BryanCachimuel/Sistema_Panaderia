f90ab2b96dda6a881fbf2913968210bc7896db59
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-10-04 20:41:59 -0500

    realizando el script de las rutas que se deseen tener para el proyecto

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
-    Route::get("/", function() {
-        return "Hola Deplynautas";
-    });
+    Route::get("/", ControladorUsuarios::class);
+    Route::get("/listar_usuarios", ControladorUsuarios::class);
 
     Route::get("/saludame/:nombre", function($nombre, Request $request) {
         return "Hola " . $nombre . " tu edad es " . $request->edad . " años.";
