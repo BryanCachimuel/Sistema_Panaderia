0e6f68d88b089a4eef28a91c2461a36d40de66f4
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-10-04 20:48:02 -0500

    creando la función includeFile el cual hace la validación de los tipos de extención de la vistas del proyecto

--- src/bin/View.php
+++ src/bin/View.php
@@ -19,6 +19,29 @@
             return $output;
         }
 
+        public function includeFile($file){
+            if(isset($this->variables) && is_array($this->variables)){
+                foreach($this->variables as $key => $value){
+                    global ${$key};
+                    ${$key} = $value;
+                }
+            }
+            if(file_exists(($file))){
+                return include $file;
+            }else
+            if(file_exists($file . ".php")){
+                return include $file . ".php";
+            }else
+            if(file_exists($file . ".html")){
+                return include $file . ".html";
+            }else
+            if(file_exists($file . ".htm")){
+                return include $file . "htm";
+            }else{
+                echo "<h2>No existe el archivo: $file</h2></br>";
+            }
+        }
+
     }
 
 ?>
