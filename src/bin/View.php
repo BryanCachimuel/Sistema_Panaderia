4829bec5b453da8a495754ffb8d657aaa84604be
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-10-04 20:46:22 -0500

    creando la clase View donde se han determinado variables, un constructor y una función llamada render

--- src/bin/View.php
+++ src/bin/View.php
@@ -2,7 +2,22 @@
 
     class View {
 
-       
+        protected $variables;
+        protected $output;
+
+        function __construct(){
+            
+        }
+
+        public function render($file, $variables=null){
+            $this->variables = $variables;
+            $file = PATH_VIEWS . $file;
+            ob_start();
+            $this->includeFile($file);
+            $output = ob_get_contents();
+            ob_end_clean();
+            return $output;
+        }
 
     }
 
