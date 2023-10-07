2785bf234c15358c0f115012196b50374e069930
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-10-06 21:33:15 -0500

    creando el objeto callback el cual esta conformado por objetos como eventos y peticiones

--- assets/js/modulos/lista.usuarios.js
+++ assets/js/modulos/lista.usuarios.js
@@ -11,6 +11,35 @@ var vista = {
     eventos: function(){
 
     },
+
+    callbacks: {
+        eventos: {
+
+        },
+
+        peticiones: {
+            listarUsuarios: {
+                beforeSend: function(){
+                    var tbody = vista.controles.tbodyListaUsuarios;
+                    tbody.html(vista.utils.templates.consultando());
+                },
+
+                completo: function(respuesta){
+                    var tbody = vista.controles.tbodyListaUsuarios;
+                    var datos = __app.parsearRespuesta(respuesta);
+                    if(datos && datos.length > 0){
+                        tbody.html('');
+                        for (var i = 0; i < datos.length; i++) {
+                            var dato = datos[i];
+                            tbody.append(vista.utils.templates.item(dato));
+                        }
+                    }else{
+                        tbody.html(vista.utils.templates.noHayRegistros());
+                    }
+                }
+            }
+        }
+    },
 };
 
 $(vista.init);
