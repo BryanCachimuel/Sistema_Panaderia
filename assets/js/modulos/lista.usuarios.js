3686c0cfbc35743facbe91a3e5b07e252a71cdd0
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-10-09 20:08:51 -0500

    creando el objeto vista el cual tendrá integrado en su interiorobjetos y métodos los cuales realizaran el proceso de listado de los usuarios registrados en la base de datos

--- assets/js/modulos/lista.usuarios.js
+++ assets/js/modulos/lista.usuarios.js
@@ -2,47 +2,41 @@ var vista = {
     controles: {
         tbodyListaUsuarios: $('#tablaListaUsuarios tbody')
     },
-
-    init: function(){
+    init: function () {
         vista.eventos();
         vista.peticiones.listarUsuarios();
     },
-
-    eventos: function(){
+    eventos: function () {
 
     },
-
     callbacks: {
         eventos: {
 
         },
-
         peticiones: {
             listarUsuarios: {
-                beforeSend: function(){
+                beforeSend: function () {
                     var tbody = vista.controles.tbodyListaUsuarios;
                     tbody.html(vista.utils.templates.consultando());
                 },
-
-                completo: function(respuesta){
+                completo: function (respuesta) {
                     var tbody = vista.controles.tbodyListaUsuarios;
                     var datos = __app.parsearRespuesta(respuesta);
-                    if(datos && datos.length > 0){
+                    if (datos && datos.length > 0) {
                         tbody.html('');
                         for (var i = 0; i < datos.length; i++) {
                             var dato = datos[i];
                             tbody.append(vista.utils.templates.item(dato));
                         }
-                    }else{
+                    } else {
                         tbody.html(vista.utils.templates.noHayRegistros());
                     }
                 }
             }
         }
     },
-
     peticiones: {
-        listarUsuarios: function(){
+        listarUsuarios: function () {
             __app.get(RUTAS_API.USUARIOS.LISTAR)
                     .beforeSend(vista.callbacks.peticiones.listarUsuarios.beforeSend)
                     .success(vista.callbacks.peticiones.listarUsuarios.completo)
@@ -50,31 +44,29 @@ var vista = {
                     .send();
         }
     },
-
     utils: {
         templates: {
-            item: function(obj){
+            item: function (obj) {
                 return '<tr>'
-                       + '<td>'+obj.nombres+'</td>'
-                       + '<td>'+obj.apellidos+'</td>'
-                       + '<td>'+obj.edad+'</td>'
-                       + '<td>'+obj.correo+'</td>'
-                       + '<td>'+obj.telefono+'</td>'
-                       + '<td>'
-                       + '<a href="javascript:;" class="btn-accion editar">Editar</a>'
-                       + '  |  '
-                       + '<a href="javascript:;" class="btn-accion eliminar">Eliminar</a>'
-                       + '<td>'
-                       + '</tr>';
+                        + '<td>' + obj.nombres + '</td>'
+                        + '<td>' + obj.apellidos + '</td>'
+                        + '<td>' + obj.edad + '</td>'
+                        + '<td>' + obj.correo + '</td>'
+                        + '<td>' + obj.telefono + '</td>'
+                        + '<td>'
+                        + '<a href="javascript:;" class="btn-accion editar">Editar</a>'
+                        + '  |  '
+                        + '<a href="javascript:;" class="btn-accion eliminar">Eliminar</a>'
+                        + '</td>'
+                        + '</tr>';
             },
-            consultando: function(){
-                return '<tr><td colspan="6">Consultando...</td></tr>';
+            consultando: function () {
+                return '<tr><td colspan="6">Consultando...</td></tr>'
             },
-            noHayRegistros: function(){
-                return '<tr><td colspan="6">No hay Registros...</td></tr>';
+            noHayRegistros: function () {
+                return '<tr><td colspan="6">No hay registros...</td></tr>';
             }
         }
-    }
+    },
 };
-
 $(vista.init);
