24b9859f0801d7f5ffceaa37ab8686852c2849bf
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-10-10 19:17:11 -0500

    creando el objeto callbacks el cual tendrá objetos internos como es eventos y accionesFormRegistro

--- assets/js/modulos/registrar.usuarios.js
+++ assets/js/modulos/registrar.usuarios.js
@@ -8,5 +8,18 @@ var vista = {
     eventos: function () {
         vista.controles.formUsuario.on('submit', vista.callbacks.eventos.accionesFormRegistro.ejecutar);
     },
+    callbacks: {
+        eventos: {
+            accionesFormRegistro: {
+                ejecutar: function (evento) {
+                    __app.detenerEvento(evento);
+                    var form = vista.controles.formUsuario;
+                    var obj = form.getFormData();
+                    console.log(obj);
+                    vista.peticiones.registrarUsuario(obj);
+                }
+            }
+        },  
+    },
 };
 $(vista.init);
