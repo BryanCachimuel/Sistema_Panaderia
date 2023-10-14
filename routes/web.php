30a7307e8fe25dc0dc890b611b94000db5a4b0db
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-10-13 21:47:55 -0500

    creando la ruta para realizar el proceso de edición

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 Route::get("/", ControladorUsuarios::class);
 Route::get("/listar_usuarios", ControladorUsuarios::class);
 Route::get("/usuarios/form/crear", ControladorUsuarios::class."@formCrearUsuario");
+Route::get("/usuarios/form/edicion/:id", ControladorUsuarios::class."@formEdicionUsuario");
 
 Route::post("/usuarios/registrar", ControladorUsuarios::class."@insertarUsuario");
