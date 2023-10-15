744bc680bbe29b1f048ba03dff675a14262748e7
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-10-14 21:36:51 -0500

    agregando al objeto USUARIOS las claves y valores para la actualización y consulta por id de usuarios

--- assets/js/global/rutas.api.js
+++ assets/js/global/rutas.api.js
@@ -2,5 +2,7 @@ var RUTAS_API = {
     USUARIOS: {
         LISTAR: 'listar_usuarios',
         REGISTRAR_USUARIO: 'usuarios/registrar',
+        CONSULTAR_USUARIO_POR_ID: 'usuarios/consultarUsuarioPorId',
+        ACTUALIZAR_USUARIO: 'usuarios/actualizar'
     }
 };
